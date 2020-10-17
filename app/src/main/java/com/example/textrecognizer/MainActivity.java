a55b1f3e60dc11b5cf36608400c8996c7b6376ee
Aditya Sharma <adityasharma5119@gmail.com>
2020-10-17 19:56:39 +0530

    UI

--- app/src/main/java/com/example/textrecognizer/MainActivity.java
+++ app/src/main/java/com/example/textrecognizer/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.textrecognizer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button selectImage;
+    private TextView extractedView;
+    private ImageView selectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        selectImage=findViewById(R.id.button);
+        extractedView=findViewById(R.id.textView);
+        selectedImage=findViewById(R.id.imageView);
     }
 }
