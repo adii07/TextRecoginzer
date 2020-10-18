56982dc4298c45f2df21976ae023ce406aee6d0a
Aditya Sharma <adityasharma5119@gmail.com>
2020-10-18 13:48:14 +0530

    logic

--- app/src/main/java/com/example/textrecognizer/MainActivity.java
+++ app/src/main/java/com/example/textrecognizer/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.textrecognizer;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     private Button selectImage;
-    private TextView extractedView;
+    private TextView extractedText;
     private ImageView selectedImage;
+    public static final int PIC_IMAGE=121;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +33,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         selectImage=findViewById(R.id.button);
-        extractedView=findViewById(R.id.textView);
+        extractedText=findViewById(R.id.textView);
         selectedImage=findViewById(R.id.imageView);
+
+
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Image"),PIC_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==PIC_IMAGE){
+            selectedImage.setImageURI(data.getData());
+            FirebaseVisionImage image;
+            try {
+                image = FirebaseVisionImage.fromFilePath(getApplicationContext(), data.getData());
+                FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance()
+                        .getOnDeviceTextRecognizer();
+
+
+
+                textRecognizer.processImage(image)
+                        .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText result) {
+                                // Task completed successfully
+                                // ...
+                                extractedText.setText(result.getText());
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                    }
+                                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
